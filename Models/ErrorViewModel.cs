819f477c81d4b572baf8f32744391ebba05f2041
Manjunadhan <agentdevil45@gmail.com>
2023-11-10 18:22:17 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace admin_admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
